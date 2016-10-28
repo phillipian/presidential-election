6415bd2229e059d2f724912022542eeca62fd9e0
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-10-27 23:08:29 -0400

    More changes

--- js/menu.js
+++ js/menu.js
@@ -21,26 +21,38 @@ $(function() {
     if (selected === "#students-faculty") {
       var clinton = 76;
       var trump = 13;
+      var stein = 73;
+      var johnson = 15;
       $('.candidate.clinton .text h1').text(`${clinton}%`);
       $('.candidate.trump .text h1').text(`${trump}%`);
+      $('.other-candidates .stein h1').text(`${stein}%`);
+      $('.other-candidates .johnson h1').text(`${johnson}%`);
       $('.bar').removeClass().addClass('bar');
       $('.bar').addClass('student-faculty-popular');
     }
     else if (selected === "#students") {
-      var clinton = 95.3;
-      var trump = 4.7;
+      var clinton = 73;
+      var trump = 15;
+      var stein = 5;
+      var johnson = 7;
       $('.candidate.clinton .text h1').text(`${clinton}%`);
       $('.candidate.trump .text h1').text(`${trump}%`);
+      $('.other-candidates .stein h1').text(`${stein}%`);
+      $('.other-candidates .johnson h1').text(`${johnson}%`);
       $('.bar').removeClass().addClass('bar');
       $('.bar').addClass('students-popular');
     }
     else if (selected === "#faculty") {
-      var clinton = 95.3;
-      var trump = 4.7;
+      var clinton = 94;
+      var trump = 4;
+      var stein = 0;
+      var johnson = 2;
       $('.candidate.clinton .text h1').text(`${clinton}%`);
       $('.candidate.trump .text h1').text(`${trump}%`);
+      $('.other-candidates .stein h1').text(`${stein}%`);
+      $('.other-candidates .johnson h1').text(`${johnson}%`);
       $('.bar').removeClass().addClass('bar');
-      $('.bar').addClass('students-popular');
+      $('.bar').addClass('faculty-popular');
     }
     //
     // var $charts = $(this).parent().parent().parent().parent().find('.panes div');
